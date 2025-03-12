1be0fe766146019073436d5d507887035881afe9
Jacqueline <jcasalia@yahoo.com.br>
2025-03-12 13:57:16 -0300

    iniciando o projeto

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+
+
+import AppRoutes from "./Routes/Routes"
+
+
+
+function App() {
+ 
+
+  return (
+
+
+  <AppRoutes/>
+   
+  
+  )
+}
+
+export default App
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+
+
+export default function Home() {
+  
+
+
+  return (
+    
+< >
+
+
+
+   <div className="main">
+iniciando o projeto
+
+   </div>
+
+</>
+  )
+ 
+  
+}
